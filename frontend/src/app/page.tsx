701c3c3bb0319bf2fd8215c50afd350fc5238664
Benjamin Le <benjaminle2002@gmail.com>
2024-09-01 15:02:51 -0400

    mockup landing

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -11,19 +11,20 @@ const SearchBar = () => {
         alt="Search icon"
         priority
       />
+      <input type="text" placeholder="Search for tarpit"></input>
     </div>
   );
 };
 
 const Result = () => {
   return (
-    <div className={styles.search_result}>
+    <li className={styles.result}>
       <div className={styles.result_header}>
         <div className={styles.flex_baseline}>
           <p style={{ marginRight: "5px" }}>Tarpit #123:</p>
           <h3>Digitalized Nameplate</h3>
         </div>
-        <div className={styles.flex_baseline}>
+        <div className={styles.flex_baseline} style={{ marginRight: "10px" }}>
           <Image
             src="/geopin-black.png"
             width={14}
@@ -41,7 +42,7 @@ const Result = () => {
           a website for people to create books about their loved ones’ stories
         </p>
       </div>
-    </div>
+    </li>
   );
 };
 
@@ -49,27 +50,13 @@ const SearchResultList = () => {
   return (
     <div className={styles.search_list}>
       <ul>
-        <li>
-          <Result />
-        </li>
-        <li>
-          <Result />
-        </li>
-        <li>
-          <Result />
-        </li>
-        <li>
-          <Result />
-        </li>
-        <li>
-          <Result />
-        </li>
-        <li>
-          <Result />
-        </li>
-        <li>
-          <Result />
-        </li>
+        <Result />
+        <Result />
+        <Result />
+        <Result />
+        <Result />
+        <Result />
+        <Result />
       </ul>
     </div>
   );
@@ -95,17 +82,27 @@ const Info = () => {
         priority
       />
       <p className={styles.logo_title}>Tarpits</p>
-      <p className={styles.info_desc}>
-        Tarpits are ideas that on the surface seem like fantastic opportunities
-        but is actually a trap for entrepreneurs. Many flock to them for its
-        appealing odor, often getting trapped in it, wasting precious resources
-        and time.
-        <br />
-        <br />
-        We’re collecting stories of tarpits you encountered on your business
-        journeys. We’d love to hear the ups and downs, why the idea attracted
-        you in the first place, and why you think this was a tarpit.
-      </p>
+      <div className={styles.info_desc}>
+        <p style={{ margin: "0px" }}>
+          Tarpits are ideas that on the surface seem like fantastic
+          opportunities but is actually a trap for entrepreneurs. Many flock to
+          them for its appealing odor, often getting trapped in it, wasting
+          precious resources and time.
+          <br />
+          <br />
+          We’re collecting stories of tarpits you encountered on your business
+          journeys. We’d love to hear the ups and downs, why the idea attracted
+          you in the first place, and why you think this was a tarpit.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const Menu = () => {
+  return (
+    <div className={styles.menu}>
+      <Image src="/writing.png" height={20} width={20} alt="Add" priority />
     </div>
   );
 };
@@ -114,6 +111,7 @@ export default function Home() {
     <main className={styles.container}>
       <Info />
       <Search />
+      <Menu />
     </main>
   );
 }
